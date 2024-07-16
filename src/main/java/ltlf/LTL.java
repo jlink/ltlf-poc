d4098c16636aced57a2d7e0c653075172806a3d4
Johannes Link <business@johanneslink.net>
2024-07-16 13:59:39 +0200

    A bit of documentation for LTL class

--- src/main/java/ltlf/LTL.java
+++ src/main/java/ltlf/LTL.java
@@ -2,6 +2,17 @@ package ltlf;
 
 import java.util.*;
 
+/**
+ * A Linear Temporal Logic (LTL) system with finite semantics.
+ *
+ * <p>
+ *     Finite semantics means that only finite traces are considered.
+ *     - Always holds when it holds in all states of the trace.
+ *     - Eventually holds when it holds in at least one state of the trace.
+ *     - Next does not require that a next state even exists.
+ *     - Until does not require that the second formula ever holds.
+ * </p>
+ */
 public class LTL {
 
 	private final Set<LTLFormula> formulae = new LinkedHashSet<>();
