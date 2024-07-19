79f7c12b3fda775852a9591290402c87c37cc9a6
Johannes Link <business@johanneslink.net>
2024-07-19 11:26:07 +0200

    Always now uses TraceChecker

--- src/main/java/ltlf/LTLFormula.java
+++ src/main/java/ltlf/LTLFormula.java
@@ -2,21 +2,21 @@ package ltlf;
 
 public interface LTLFormula extends TraceChecker {
 
-	static LTLFormula always(StateChecker checker) {
+	static LTLFormula always(TraceChecker checker) {
 		return new Always(checker);
 	}
 
 	class Always implements LTLFormula {
-		private final StateChecker checker;
+		private final TraceChecker checker;
 
-		public Always(StateChecker checker) {
+		public Always(TraceChecker checker) {
 			this.checker = checker;
 		}
 
 		@Override
 		public boolean check(LTLTrace trace) {
 			for (LTLState state : trace.states()) {
-				if (!checker.checkState(state)) {
+				if (!checker.check(LTLTrace.of(state))) {
 					return false;
 				}
 			}
