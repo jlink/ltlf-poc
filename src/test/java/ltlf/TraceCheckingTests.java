4251570fe50d6193d87a3a24c7eb8cf6b09642f5
Johannes Link <business@johanneslink.net>
2024-07-19 11:50:23 +0200

    Refactoring TracChecking tests

--- src/test/java/ltlf/TraceCheckingTests.java
+++ src/test/java/ltlf/TraceCheckingTests.java
@@ -2,47 +2,52 @@ package ltlf;
 
 import net.jqwik.api.*;
 
+import static ltlf.LTLFormula.*;
 import static ltlf.LTLState.*;
 import static ltlf.StateChecker.*;
-import static ltlf.LTLFormula.*;
 import static org.assertj.core.api.Assertions.*;
 
 class TraceCheckingTests {
 
-	@Example
-	void alwaysChecker() {
-		var always = always(fact("a"));
-		boolean matches = always.check(LTLTrace.of(
-			atoms("a", "b", "c"),
-			atoms("a", "b"),
-			atoms("a")
-		));
+	@Group
+	class Always {
 
-		assertThat(matches).isTrue();
-	}
+		@Example
+		void matches() {
+			var always = always(fact("a"));
+			boolean matches = always.check(LTLTrace.of(
+				atoms("a", "b", "c"),
+				atoms("a", "b"),
+				atoms("a")
+			));
 
-	@Example
-	void alwaysDoesNotMatch() {
-		var always = always(fact("b"));
-		boolean matches = always.check(LTLTrace.of(
-			atoms("a", "b", "c"),
-			atoms("a", "b"),
-			atoms("a")
-		));
+			assertThat(matches).isTrue();
+		}
 
-		assertThat(matches).isFalse();
-	}
+		@Example
+		void doesNotMatch() {
+			var always = always(fact("b"));
+			boolean matches = always.check(LTLTrace.of(
+				atoms("a", "b", "c"),
+				atoms("a", "b"),
+				atoms("a")
+			));
 
-	@Example
-	void nestedAlways() {
-		var always = always(always(fact("a")));
-		boolean matches = always.check(LTLTrace.of(
-			atoms("a", "b", "c"),
-			atoms("a", "b"),
-			atoms("a")
-		));
+			assertThat(matches).isFalse();
+		}
 
-		assertThat(matches).isTrue();
+		@Example
+		void nested() {
+			var always = always(always(fact("a")));
+			boolean matches = always.check(LTLTrace.of(
+				atoms("a", "b", "c"),
+				atoms("a", "b"),
+				atoms("a")
+			));
+
+			assertThat(matches).isTrue();
+		}
+
 	}
 
 }
