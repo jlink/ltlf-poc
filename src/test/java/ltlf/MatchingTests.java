ab8a3514fc20579cddc09e05fb71e91f343301a8
Johannes Link <business@johanneslink.net>
2024-07-19 10:40:12 +0200

    Restructure tests

--- src/test/java/ltlf/MatchingTests.java
+++ src/test/java/ltlf/MatchingTests.java
@@ -7,7 +7,7 @@ import static ltlf.LTLFormula.*;
 import static ltlf.LTLState.*;
 import static org.assertj.core.api.Assertions.*;
 
-class LTLTests {
+class MatchingTests {
 
 	@Example
 	void matchEmptyTrace() {
@@ -44,27 +44,6 @@ class LTLTests {
 		);
 
 		assertThat(matches).isTrue();
-	}
-
-	@Example
-	void notChecker() {
-		StateChecker notA = not(fact("a"));
-		assertThat(notA.check(atoms("b", "c"))).isTrue();
-		assertThat(notA.check(atoms("b", "a"))).isFalse();
-	}
-
-	@Example
-	void andChecker() {
-		StateChecker aAndB = fact("a").and(fact("b"));
-		assertThat(aAndB.check(atoms("b", "a"))).isTrue();
-		assertThat(aAndB.check(atoms("b", "x"))).isFalse();
-	}
-
-	@Example
-	void orChecker() {
-		StateChecker aOrB = fact("a").or(fact("b"));
-		assertThat(aOrB.check(atoms("b", "x"))).isTrue();
-		assertThat(aOrB.check(atoms("y", "x"))).isFalse();
 	}
 
 	@Example
